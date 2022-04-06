049d6fdb2b0d1efe67dacd491cfe6d3aba6ccb28
Joe C <consolinojoe@gmail.com>
2022-04-06 12:13:11 -0400

    Added login page

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-function Login() {
+const Login = props => {
+
+    const [name, setName] = useState("")
+    const [id, setId] = useState("")
+
+    const onChangeName = e => {
+        const name = e.target.value
+        setName(name);
+    }
+
+    const onChangeId = e => {
+        const id = e.target.value
+        setId(id);
+    }
+
+    const login = () => {
+        props.login({ name: name, id: id })
+        props.history.push('/')
+    }
+
     return (
-        <div className="App">
-            Login
+        <div>
+            <Form>
+                <Form.Group>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter Username"
+                        value={name}
+                        onChange={onChangeName}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>ID</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter ID"
+                        value={id}
+                        onChange={onChangeId}
+                    />
+                </Form.Group>
+                <Button variant="primary" onClick={login}>
+                    Submit
+                </Button>
+            </Form>
         </div>
-    );
+    )
+
 }
 
 export default Login;
